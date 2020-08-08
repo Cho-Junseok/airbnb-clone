36bd73394e10e7813f18d2b57bb4c389797d04ae
CHO JUNSEOK <CHOJUNSEOK@USERnoMacBook-Pro.local>
2020-08-08 19:04:31 +0900

    Room Admin Part1

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
+class RoomType(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """ RoomAdmin Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ PhotoAdmin Admin Definition """
+
+    pass
