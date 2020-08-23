2db9313d758ec7d8041cc7c983fc0e5809f8fe6f
CHO JUNSEOK <CHOJUNSEOK@USERnoMacBook-Pro.local>
2020-08-23 10:30:59 +0900

    More Admin part2

--- rooms/models.py
+++ rooms/models.py
@@ -54,7 +54,7 @@ class Photo(core_models.TimeStampedModel):
     """ HouseRule Object Definityion """
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField()
+    file = models.ImageField(upload_to="room_photos")
     rooms = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
