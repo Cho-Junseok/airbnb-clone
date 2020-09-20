932533deec095d8083eb01272b7330724634a1b6
CHO JUNSEOK <CHOJUNSEOK@USERnoMacBook-Pro.local>
2020-09-20 10:45:40 +0900

    #17 LOG IN WITH GITHUB

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     path("verify/<str:key>", views.complete_verification, name="complete-verification"),
+    path("login/github", views.github_login, name="github-login"),
+    path("login/github/callback", views.github_callback, name="github-callback"),
 ]
 
